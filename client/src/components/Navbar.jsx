13cfe2ed97f061431fc1f6290e1311a7985cc879
SagarNegi10 <sagarnegi13.0@gmail.com>
2025-08-02 19:53:58 +0530

    integrate clerk auth and add hero section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,34 @@
-import { ArrowRight } from 'lucide-react'
-import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
+import { ArrowRight } from "lucide-react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
 
   return (
-    <div className='fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
-      <img src={assets.logo} alt="logo" className='w-32 md:w-44 cursor-pointer' onClick={() => navigate('/')}/>
+    <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="w-32 md:w-44 cursor-pointer"
+        onClick={() => navigate("/")}
+      />
 
-      <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get started <ArrowRight className='w-4 h-4'/></button>
+      {user ? (
+        <UserButton />
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        >
+          Get started <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import AiTools from "../components/AiTools"
+import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
 
 
@@ -5,6 +7,8 @@ const Home = () => {
   return (
     <>
       <Navbar/>
+      <Hero/>
+      <AiTools/>
     </>
   )
 }
