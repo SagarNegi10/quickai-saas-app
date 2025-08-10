e68995e93cbd6a0a6285e39eb07e23fcce6b0a47
SagarNegi10 <sagarnegi13.0@gmail.com>
2025-08-10 15:56:55 +0530

    fix issue

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
   return (
     <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen">
-      <div className="text-center mb-6">
+      <div className="mt-15 text-center mb-6">
         <h1 className="text-4xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]">
           Create Stunning Content <br /> with{" "}
           <span className="text-primary">AI tools</span>
